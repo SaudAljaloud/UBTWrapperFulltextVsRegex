5674e147ede1935b691cd27ddc16b501a044dc90
SaudAljaloud <sza1g10@ecs.soton.ac.uk>
2013-11-19 21:42:18 +0000

    mod run

--- UBTWrapperVirtuoso7.0/run_ubt.sh
+++ UBTWrapperVirtuoso7.0/run_ubt.sh
@@ -1,1 +1,1 @@
-java -Xmx512m -Djava.util.logging.config.file=etc/jdk14.properties -cp ../UBT/ubt.jar:ubtwrapper-virtuoso-7.0.0.jar:lib/virtjdbc4.jar:lib/virtjdbc4ssl.jar:lib/slf4j-api-1.5.0.jar:lib/slf4j-jdk14-1.5.0.jar edu.lehigh.swat.bench.ubt.Test $1 $2 $3
+java -Xmx512m -Djava.util.logging.config.file=etc/jdk14.properties -cp target/UBTWrapperVirtuoso7.0-0.0.1.jar:target/dependency/*:$CLASSPATH edu.lehigh.swat.bench.ubt.Test $1 $2 $3

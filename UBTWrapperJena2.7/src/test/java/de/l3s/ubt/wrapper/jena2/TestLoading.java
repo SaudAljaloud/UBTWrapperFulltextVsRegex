ff569a4913b4c0399f9bd72f4d75a51ffd36d18f
SaudAljaloud <sza1g10@ecs.soton.ac.uk>
2013-12-07 22:45:40 +0000

    changing package order

---
+++ UBTWrapperJena2.7/src/test/java/de/l3s/ubt/wrapper/jena2/TestLoading.java
@@ -0,0 +1,22 @@
+package de.l3s.ubt.wrapper.jena2;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+
+public class TestLoading {
+
+	/**
+	 * @param args
+	 */
+	@Test
+	public void testPrintHelloWorld2() {
+ 
+		Boolean a = true;
+		Boolean b = false ;
+		Assert.assertEquals(a, b);
+ 
+	}
+
+}
---
+++ UBTWrapperJena2.7/src/test/java/de/l3s/ubt/wrapper/jena2/TestLoading4.java
@@ -0,0 +1,15 @@
+package de.l3s.ubt.wrapper.jena2;
+
+import org.junit.Test;
+
+public class TestLoading4 {
+
+	
+	
+	@Test
+	public void saud(){
+		
+		
+	}
+
+}

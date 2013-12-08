bc84196a1281ce633714f49591c0b46b9b0ed748
SaudAljaloud <sza1g10@ecs.soton.ac.uk>
2013-12-08 19:21:26 +0000

    adding test suite

--- UBTWrapperJena2.7/src/test/java/de/l3s/ubt/wrapper/jena2/TestLoading.java
+++ UBTWrapperJena2.7/src/test/java/de/l3s/ubt/wrapper/jena2/TestLoading.java
@@ -1,22 +1,48 @@
 package de.l3s.ubt.wrapper.jena2;
 
+
+import org.apache.log4j.BasicConfigurator;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runners.Suite;
 
 
+import edu.lehigh.swat.bench.ubt.RepositoryCreator;
 
-public class TestLoading {
+
+import edu.lehigh.swat.bench.ubt.api.Repository;
+public class TestLoading extends RepositoryCreator {
+	public Repository repository_ = null;
+
 
 	/**
 	 * @param args
 	 */
 	@Test
-	public void testPrintHelloWorld2() {
- 
-		Boolean a = true;
-		Boolean b = false ;
-		Assert.assertEquals(a, b);
- 
+	public void testLoadingJena2_7() {
+
+		ConfigMock kb = new ConfigMock();
+		Boolean laodResult = false;
+		try {
+			repository_ = createRepository(kb.className);
+			if (repository_ == null) {
+				System.err.println(kb.className + ": class not found!");
+				System.exit(-1);
+			}
+
+			repository_.setOntology(kb.ontologyName);
+			repository_.open(kb.databaseName);
+			if (!repository_.load(kb.dataSource)) {
+				repository_.close();
+
+			}
+			laodResult = true;
+		} catch (Exception e) {
+			laodResult = false;
+		}
+
+		Assert.assertEquals(true, laodResult);
+		
 	}
 
 }

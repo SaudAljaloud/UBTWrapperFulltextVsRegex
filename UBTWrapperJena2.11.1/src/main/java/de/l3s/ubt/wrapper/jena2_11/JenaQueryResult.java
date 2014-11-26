81bb551fc21f912a2fc0226458f2402c65d70151
SaudAljaloud <mr123456>
2014-11-26 11:47:27 +0000

    no message

--- UBTWrapperJena2.11.1/src/main/java/de/l3s/ubt/wrapper/jena2_11/JenaQueryResult.java
+++ UBTWrapperJena2.11.1/src/main/java/de/l3s/ubt/wrapper/jena2_11/JenaQueryResult.java
@@ -1,3 +1,15 @@
+/**
+
+* Copyright 2012 Saud Aljaloud
+* 
+* This file is part of UBTWrapperFulltextVsRegex.
+* 
+* UBTWrapperFulltextVsRegex is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+* 
+* UBTWrapperFulltextVsRegex is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License along with UBTWrapperFulltextVsRegex. If not, see http://www.gnu.org/licenses/.
+ */
 package de.l3s.ubt.wrapper.jena2_11;
 
 import com.hp.hpl.jena.query.ResultSet;
